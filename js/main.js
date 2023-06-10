67ffe9d0340dc7a6b484ef586b4c8de1d533b616
unknown <haythamgamal6@gmail.com>
2023-06-11 00:31:18 +0300

    21

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ let upper = document.querySelector(".upper-bar");
 let nav = document.querySelector(".navbar");
 let slider = document.querySelector(".slider");
 let carousel = document.querySelector(".carousel-inner");
+let gallery = document.querySelector(".gallery");
+let links = document.querySelectorAll(".links ul li");
 
 (windowinnerHeight = window.innerHeight),
   (getHeight = windowinnerHeight - (upper.offsetHeight + nav.offsetHeight));
 slider.style.height = `${getHeight}px`;
 carousel.style.height = `${getHeight}px`;
+
